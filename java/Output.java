c90d35bf2d5fc1038dc1bae8f91475a5399e0e03
pooja-maane <poojamaane7056@gmail.com>
2024-12-30 22:22:37 +0530

    add java programming language

---
+++ java/Output.java
@@ -0,0 +1,7 @@
+class Output {
+    public static void main(String args[]) {
+        System.out.println("this is first line");
+        System.out.print("this is second line\n");
+        System.out.print("this is third line");
+    }
+}
